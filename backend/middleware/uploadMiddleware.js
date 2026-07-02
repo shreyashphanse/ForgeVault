db4eac9efcc2dc8b3ccad0c052612cb58c354306
shreyash-demo <shreyashphanse22@gmail.com>
2026-07-03 01:40:35 +0530

    fixed storage directories for uploads

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,13 +1,21 @@
 const multer = require("multer");
 const path = require("path");
 
+const fs = require("fs");
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    let uploadPath;
+
     if (file.fieldname === "logo") {
-      cb(null, "storage/logos");
+      uploadPath = path.join(__dirname, "..", "storage", "logos");
     } else if (file.fieldname === "apk") {
-      cb(null, "storage/apks");
+      uploadPath = path.join(__dirname, "..", "storage", "apks");
     }
+
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    cb(null, uploadPath);
   },
 
   filename: (req, file, cb) => {
